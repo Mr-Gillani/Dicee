38600c5b443f0e47f740feff3c082714b9cb87d3
ZooHippo <58636225+ZooHippo@users.noreply.github.com>
2020-03-01 14:11:48 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+function diceeRoll(){
+  var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
+
+  var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
+
+  var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
+
+  var image1 = document.querySelectorAll("img")[0];
+
+  image1.setAttribute("src", randomImageSource);
+
+
+  var randomNumber2 = Math.floor(Math.random() * 6) + 1; //1-6
+
+  var randomDiceImage = "dice" + randomNumber2 + ".png"; //dice1.png - dice6.png
+
+  var randomImageSource = "images/" + randomDiceImage;
+
+  var image2 = document.querySelectorAll("img")[1];
+
+  image2.setAttribute("src", randomImageSource);
+
+  if (randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML = "&#x1F6A9; Player 1 Wins"
+  }
+
+  else if (randomNumber2 > randomNumber1){
+    document.querySelector("h1").innerHTML = "Player 2 Wins &#x1F6A9;"
+  }
+  else{
+    document.querySelector("h1").innerHTML = "Draw &#x1F91E;"
+  }
+}
